d8ef46706b14450e37cb57dfb0d8ed15a5045896
ashu <ashutoshmeher885@gmail.com>
2025-12-11 20:01:20 +0530

    Add Ashu component and update App to include it

--- React/01basicreact/src/App.js
+++ React/01basicreact/src/App.js
@@ -1,6 +1,11 @@
+import Ashu from './ashu.js';
+
 function App() {
   return (
-    <h1>Ashu padh raha React</h1>
+    <>
+    <h1>Ashu padh raha hai React</h1>
+    <Ashu />  
+    </>
   );
 }
 
---
+++ React/01basicreact/src/ashu.js
@@ -0,0 +1,10 @@
+function Ashu(){  //always captalize the first letter of component name and import as such
+    return (
+        <>
+            <h3>React is thoda sa tough</h3>
+            <p>Test paragraph</p>
+        </>
+    )
+}
+
+export default Ashu;

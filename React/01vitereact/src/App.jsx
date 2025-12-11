32cdf219c2614fe5454197033fa9ad468bfe4a87
ashu <ashutoshmeher885@gmail.com>
2025-12-11 19:47:29 +0530

    Add Ashu component and update App to render it

--- React/01vitereact/src/App.jsx
+++ React/01vitereact/src/App.jsx
@@ -1,8 +1,15 @@
+import Ashu from './ashu.jsx'
+
 function App() {
 
   return (
-    <h1>Ashu padh raha Vite + React!</h1>
+    // <h1>Ashu padh raha Vite + React!</h1>
+    <>
+      <Ashu />
+      <h3>From Chai and Code</h3>
+      <p>Learning React is fun but confusing😖! // test paragrah</p>
+    </>
   )
-}
+} // can return only one element thats why we enclose in a div tag or fragment [<> </>]
 
 export default App
---
+++ React/01vitereact/src/ashu.jsx
@@ -0,0 +1,7 @@
+function Ashu(){
+    return(
+        <h2> Ashu aa gaya </h2>
+    )
+}
+
+export default Ashu

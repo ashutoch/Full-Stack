982631d36a09f5ccee4a589932197aef3c4784f3
ashu <ashutoshmeher885@gmail.com>
2026-02-05 22:02:27 +0530

    feat: update User component to accept user object and display user details

--- React/05props/src/App.jsx
+++ React/05props/src/App.jsx
@@ -2,12 +2,29 @@ import { useState } from 'react'
 import User from './User'
 
 function App() {
-    let name = "Ashu"
+    // let name = "Ashu", age = 20, email = "ashu@gmail.com";
+    // let userName = "Ashutosh";
+    // let aage = 69;
+    // let gmail = "ashu885@gmail.com";
+
+    let userObject = {
+        userName: "Ashutosh",
+        age: 5,
+        gmail: "ashu@gmail.com"
+    }
+
     return (
         <div>
             <h1>Props in React JS</h1>
-            <User name = {name}/>  
+            {/* <User name = {name} age = {age} email = {email}/>   */}
             {/* keep it inside {}. it will work normally too but we need {} for variables and values so its better to use it by default */}
+
+            {/* <User name = {userName} age = {aage} email = {gmail} />  */}
+            {/* //!passing as variables */}
+
+            <User user = {userObject} />
+
+
         </div>
     )
 }
--- React/05props/src/User.jsx
+++ React/05props/src/User.jsx
@@ -1,9 +1,34 @@
-function User(props){  // we can also pass it inside the function itself using {name} like function User({name}) but remember to use the exact name of the parameter that you have written, otherwise it wont work
-    // console.log(props) // it will show the whole key and value 
-    // console.log(props.name) // it will only show the value 
+// function User(props){  //! we can also pass it inside the function itself using {name} like function User({name}) but remember to use the exact name of the parameter that you have written, otherwise it wont work unless you have passed it
+     // console.log(props) //! it will show the whole key and value 
+     // console.log(props.name) //! it will only show the value 
+//     return (
+//         <div>
+//             <h2>User Component</h2>
+//             <h4>Name: {props.name}</h4>
+//             <h4>Age: {props.age}</h4>
+//             <h4>Email: {props.email}</h4>
+//         </div>
+//     )
+// }
+
+// function User({name, age, email}){  //! we can also pass it inside the function itself
+//     return (
+//         <div>
+//             <h2>User Component</h2>
+//             <h4>Name: {name}</h4>
+//             <h4>Age: {age}</h4>
+//             <h4>Email: {email}</h4>
+//         </div>
+//     )
+// }
+
+function User({user}){
     return (
         <div>
-            <h3>User Component</h3>
+            <h2>User Component</h2>
+            <h4>Name: {user.userName}</h4>
+            <h4>Age: {user.age}</h4>
+            <h4>Email: {user.gmail}</h4>
         </div>
     )
 }
